b52f8c13d87c2f09cb70520727ea5e38c990d847
Ronny Ancorini <Ronnygx@gmail.com>
2015-11-22 21:57:21 +0000

    Rules to develop

---
+++ GameOfLife.Tests/RulesTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+	[TestFixture]
+	public class RulesTests
+	{
+		// TODO:
+		//   - Get an alive cell when the given one has 3 neighbors	 
+		//   - Get a dead cell when the given one has more than 3 neighbors	 
+		//   - Get a dead cell when the given one has less than 2 neighbors	 
+		//   - Get the given cell in any other scenario
+	}
+}

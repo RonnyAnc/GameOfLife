c238fc0cc05a7e9cc822057b54b4e764eb372954
Ronny Ancorini <Ronnygx@gmail.com>
2015-11-22 22:00:44 +0000

    Specify better a rule and reorder to do list

--- GameOfLife.Tests/RulesTests.cs
+++ GameOfLife.Tests/RulesTests.cs
@@ -6,9 +6,10 @@ namespace GameOfLife.Tests
 	public class RulesTests
 	{
 		// TODO:
+		//   - Get the given cell when it has 2 neighbors
 		//   - Get an alive cell when the given one has 3 neighbors	 
 		//   - Get a dead cell when the given one has more than 3 neighbors	 
 		//   - Get a dead cell when the given one has less than 2 neighbors	 
-		//   - Get the given cell in any other scenario
+
 	}
 }

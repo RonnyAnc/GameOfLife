962ae608075db25dbf79e910c254461a211b64ac
Ronny Ancorini <Ronnygx@gmail.com>
2015-12-02 20:51:41 +0000

    green: apply_the_one_neighbour_rule

---
+++ GameOfLife.Tests/RuleEngineShould.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using GameOfLife.rules;
+using NUnit.Framework;
+using static GameOfLife.CellState;
+
+namespace GameOfLife.Tests
+{		
+	[TestFixture]
+	public class RuleEngineShould
+	{
+		private List<Rule> ruleSet;
+		private RuleEngine ruleEngine;
+
+		[SetUp]
+		public void Setup()
+		{
+			ruleSet = new List<Rule>
+			{
+				new ACellDeadWhenHasOneNeighbor(),
+				new ACellDeadWhenHasMoreThanThreeNeighbors(),
+				new ACellLiveWhenHasThreeNeighbors(),
+				new ACellMantainsItsStateWhenHasTwoNeighborsRule()
+			};
+			ruleEngine = new RuleEngine(ruleSet);
+		}
+
+		[Test]
+		public void apply_the_one_neighbour_rule()
+		{
+			var cell = new Cell("AnyId", new List<string>());
+			cell.Configure(1);
+
+			var newState = ruleEngine.ApplyRulesTo(cell);
+
+			newState.Should().Be(Dead);
+		}
+
+	}
+}
---
+++ GameOfLife/RuleEngine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using GameOfLife.rules;
+
+namespace GameOfLife
+{
+	public class RuleEngine
+	{
+		private readonly List<Rule> ruleSet;
+
+		public RuleEngine(List<Rule> ruleSet)
+		{
+			this.ruleSet = ruleSet;
+		}
+
+		public CellState ApplyRulesTo(Cell cell)
+		{
+			var cellState = cell.State;
+			foreach (var rule in ruleSet)
+			{
+				if(cellState == cell.State)
+					cellState = rule.NextStateFor(cell);
+			}
+
+			return cellState;
+		}
+	}
+}

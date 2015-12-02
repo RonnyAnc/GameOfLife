b8542ea3ec32719ae303242b65fed2d49c44eb99
Ronny Ancorini <Ronnygx@gmail.com>
2015-12-02 21:01:45 +0000

    Pass rule tests to the Rule Engine level

--- GameOfLife.Tests/RuleEngineShould.cs
+++ GameOfLife.Tests/RuleEngineShould.cs
@@ -26,7 +26,7 @@ namespace GameOfLife.Tests
 		}
 
 		[Test]
-		public void apply_the_one_neighbour_rule()
+		public void get_dead_state_when_a_cell_has_1_neighbour()
 		{
 			var cell = new Cell("AnyId", new List<string>());
 			cell.Configure(1);
@@ -36,5 +36,48 @@ namespace GameOfLife.Tests
 			newState.Should().Be(Dead);
 		}
 
+		[Test]
+		public void keep_alive_state_when_an_alive_cell_has_2_neighbours()
+		{
+			var cell = new Cell("AnyId", new List<string>());
+			cell.Configure(2);
+
+			var newState = ruleEngine.ApplyRulesTo(cell);
+
+			newState.Should().Be(cell.State);
+		}
+
+		[Test]
+		public void keep_dead_state_when_a_dead_cell_has_2_neighbours()
+		{
+			var cell = new Cell("AnyId", new List<string>(), Dead);
+			cell.Configure(2);
+
+			var newState = ruleEngine.ApplyRulesTo(cell);
+
+			newState.Should().Be(cell.State);
+		}
+
+		[Test]
+		public void get_alive_state_when_a_cell_has_3_neighbours()
+		{
+			var cell = new Cell("AnyId", new List<string>());
+			cell.Configure(3);
+
+			var newState = ruleEngine.ApplyRulesTo(cell);
+
+			newState.Should().Be(Alive);
+		}
+
+		[Test]
+		public void get_dead_state_when_a_cell_has_more_than_3_neighbours()
+		{
+			var cell = new Cell("AnyId", new List<string>());
+			cell.Configure(4);
+
+			var newState = ruleEngine.ApplyRulesTo(cell);
+
+			newState.Should().Be(Dead);
+		}
 	}
 }
